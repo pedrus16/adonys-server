3047dd0aa72179a3f8a6b9748dea8386bfb628a7
party <party>
2015-11-25 16:13:17 +0100

    user login

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+
+}
